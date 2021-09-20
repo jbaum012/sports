6c441c5dde90e5907c198cd1f85ed51c332c71a4
John Lieb-Bauman <jliebbauman@dealerinspire.com>
2021-09-20 11:20:57 -0500

    use the correct property

--- app/Models/SportsBet.php
+++ app/Models/SportsBet.php
@@ -50,7 +50,7 @@ class SportsBet extends Model
         $authUser = Auth::check()
             ? Auth::user()->id
             : 0;
-        $bonus = $this->game->user_id === $authUser
+        $bonus = $this->game->created_by === $authUser
             ? self::CREATOR_BONUS
             : 0;
         $multiplyer = $this->doubled ? 2 : 1;
